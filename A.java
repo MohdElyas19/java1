2ada4e8b79f07c20eae35da60ad234c00a922c8e
MohdElyas19 <mdiliyaskhan21@gmail.com>
2022-05-21 19:20:36 +0530

    updated first java file

---
+++ A.java
@@ -0,0 +1,6 @@
+class Elyas {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to elyas");
+	}
+}
---
+++ elyas.java
@@ -0,0 +1,5 @@
+class Elyas {
+	public static void main(String[] args) {
+ 	System.out.println("Hi How Are You");
+	}
+}
