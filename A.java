586550bbcaf31882ffe82eea6e856076f9f447f4
MohdElyas19 <mdiliyaskhan21@gmail.com>
2022-05-21 20:10:25 +0530

    updated

--- A.java
+++ A.java
@@ -1,6 +1,17 @@
 class Elyas {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
-		System.out.println("Welcome to elyas");
+	}
+}
+
+class Elyas2 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java2");
+	}
+}
+
+class Elyas3 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java3");
 	}
 }
--- elyas.java
+++
@@ -1,5 +0,0 @@
-class Elyas {
-	public static void main(String[] args) {
- 	System.out.println("Hi How Are You");
-	}
-}
